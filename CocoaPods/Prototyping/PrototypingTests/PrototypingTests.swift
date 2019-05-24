23376f28d66bce3460943d099f3b0174fb6594aa
Andrzej Michnia <amichnia@gmail.com>
2019-05-25 00:56:41 +0200

    Added pods prototyping example

---
+++ CocoaPods/Prototyping/PrototypingTests/PrototypingTests.swift
@@ -0,0 +1,23 @@
+//
+//  PrototypingTests.swift
+//  PrototypingTests
+//
+//  Created by Andrzej Michnia on 25/05/2019.
+//  Copyright © 2019 MakeAWishFoundation. All rights reserved.
+//
+
+import XCTest
+import SwiftyMocky
+@testable import Prototyping
+
+class UnitTests: XCTestCase {
+
+    func testExample() {
+        let mock = NumberGeneratorMock()
+//        Given(mock, .next(willReturn: 1,2,3))
+        XCTAssertEqual(mock.next(), 1)
+        XCTAssertEqual(mock.next(), 2)
+        XCTAssertEqual(mock.next(), 3)
+    }
+
+}
